fc8ab7d640be274f88f2973225dc49fd2190f09f
Manav Patnaik <dev.manavpatnaik@gmail.com>
2025-02-24 15:16:51 +0530

    type command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,21 @@
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+
+    private static String removeFirstWord(String s) {
+        int i = 0;
+        while (i < s.length() && s.charAt(i) != ' ') i++;
+        return s.substring(i+1);
+    }
+
+    private final static Set<String> SHELL_BUILTINS = new HashSet<>(List.of("echo", "exit", "type"));
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
+
+
 
         Scanner scanner = new Scanner(System.in);
 
@@ -13,8 +26,12 @@ public class Main {
                 scanner.close();
                 System.exit(0);
             }
-            if (input.startsWith("echo ")) System.out.println(input.substring(5));
-            else System.out.println(input + ": command not found");
+            if (input.startsWith("echo ")) System.out.println(removeFirstWord(input));
+            else if (input.startsWith("type")) {
+                String command = removeFirstWord(input);
+                if (SHELL_BUILTINS.contains(command)) System.out.println(command + " is a shell builtin");
+                else System.out.println(command + ": not found");
+            } else System.out.println(input + ": command not found");
         }
     }
 }
